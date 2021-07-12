74e24312cb6dd248af9cd72bfec3b8a89cc6f603
Yulascha <yulascha@mail.ru>
2021-07-12 20:41:05 +0300

    2. Сокобан. Тестовое задание. [#75283]

--- puzzle/src/main/java/ru/job4j/puzzle/Win.java
+++ puzzle/src/main/java/ru/job4j/puzzle/Win.java
@@ -5,4 +5,26 @@ public class Win {
         boolean rsl = true;
         return rsl;
     }
+
+    public static boolean Horizontal(int[][] board) {
+        boolean rsl = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board.length != '1') {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
+
+    public static boolean Vertical(int[][] board) {
+        boolean rsl = true;
+        for (int i = 0; i < board.length; i++) {
+            if (board.length != '1') {
+                rsl = false;
+                break;
+            }
+        }
+        return rsl;
+    }
 }
